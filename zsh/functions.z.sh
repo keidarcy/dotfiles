2b5c3ef0d74434ecc6b65fac414dfee8bfe5e81a
XING YAHAO <keidarcy.1015@gmail.com>
2020-04-17 19:39:32 +0900

    update at  in xyyolab

--- zsh/functions.z.sh
+++ zsh/functions.z.sh
@@ -6,15 +6,11 @@ tnew() {
 
 nvm() {
     if [[ $NVM_DIR ]];then
-        #[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-        # Load bash_completion
-        #[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
-        # Call nvm
         $0 "$@"
     else
         echo "🚨 Lazy loading nvm..."
         export NVM_DIR="$HOME/.nvm"
-        source $HOME/.nvm/nvm.sh
+        [[ -f $HOME/.nvm/nvm.sh ]] && source $HOME/.nvm/nvm.sh
         echo "😚 try 'nvm' again..."
     fi
 }
