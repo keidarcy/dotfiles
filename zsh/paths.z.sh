eb00cdb419b9536a3651363a82948a41478fa219
XING YAHAO <keidarcy.1015@gmail.com>
2020-08-08 15:19:06 +0900

    update 🦄 at 2020-08-08 15:19:06 ⌛️ in xyyolab  💻

--- zsh/paths.z.sh
+++ zsh/paths.z.sh
@@ -1,6 +1,6 @@
 
-export PATH="~/.composer/vendor/bin:$PATH" 
-export PATH="~/.deno/bin:$PATH" 
+export PATH="$HOME/.composer/vendor/bin:$PATH" 
+export PATH="$HOME/.deno/bin:$PATH" 
 export PATH="/usr/local/sbin:$PATH"
 export PATH="./node_modules/.bin:$PATH"
 export PATH="./vendor/bin:$PATH"
