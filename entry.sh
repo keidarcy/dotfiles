f127af764cb28f5d78d943ff3a74a827dc221125
Xing Yahao <keidarcy.1015@gmail.com>
2021-08-22 14:29:21 +0900

    update 🦄 at 2021-08-22 14:29:21 ⌛️ in xyyolabmac.local  💻

--- entry.sh
+++ entry.sh
@@ -26,7 +26,31 @@ ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 mkdir -p ~/.vim
 ln -s ~/dotfiles/vim/plugins.vim ~/.vim/plugins.vim
 
-
 # tmux
 # oh my tmux https://github.com/gpakosz/.tmux
-ln -s ~/dotfiles/tmux/.tmux.conf.local ~/.tmux/.tmux.conf.localn -s ~/dotfiles/zsh/paths.z.sh ~/Code/bin/paths.z.sh
+cd ~
+git clone https://github.com/gpakosz/.tmux.git
+ln -sf ~/dotfiles/tmux/.tmux.conf.local ~/.tmux/.tmux.conf.local
+ln -sf ~/.tmux/.tmux.conf.local ~/.tmux.conf.local
+ln -sf ~/.tmux/.tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf # reload tmux config
+
+# ln -s ~/dotfiles/zsh/paths.z.sh ~/Code/bin/paths.z.sh
+
+
+# Fonts
+###########################################
+# Fira Code https://github.com/tonsky/FiraCode
+# Meslo Nerd Font https://github.com/romkatv/powerlevel10k
+###########################################
+
+
+# mac tips
+
+# softwareupdate --install-rosetta # intall rosetta in m1 mac
+# sudo xattr -r -d com.apple.quarantine /Applications/xxxxx.app # force open app
+###### dock
+# defaults write com.apple.dock show-recents -bool no # set show-recents to false
+# defaults write com.apple.dock recent-apps -array # intentionally empty
+# defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}' # add space to dock
+# killall Dock # restart dock and load changes
