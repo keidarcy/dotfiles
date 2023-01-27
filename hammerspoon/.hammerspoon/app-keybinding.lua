ed49d759264a19e15f7fed516cfbff4cb5bf1c4e
Xing Yahao <keidarcy.1015@gmail.com>
2023-01-27 21:46:09 +0900

    chore(hammerspoon): update keybinding

--- hammerspoon/.hammerspoon/app-keybinding.lua
+++ hammerspoon/.hammerspoon/app-keybinding.lua
@@ -8,23 +8,41 @@ local chrome_keys = {
 	utils.remap({ "shift", "cmd" }, "E", utils.key_stroke({ "alt", "cmd" }, "i")),
 }
 
+local global_keys = {
+	utils.remap({ "cmd" }, "M", utils.key_stroke({}, "escape")),
+	utils.remap({ "fn" }, "f11", utils.key_stroke({ "cmd" }, "1")),
+}
+
+local apps = {
+	["com.mongodb.compass"] = true,
+	["com.google.Chrome"] = true,
+	["com.google.Chrome.canary"] = true,
+	["com.insomnia.app"] = true,
+}
+
 local function handle_app_event(_, event, app)
 	local bundleID = app:bundleID()
+	-- global keybind
+
 	-- Chrome keybind
 	if event == hs.application.watcher.activated then
-		if bundleID == "com.google.Chrome" or bundleID == "com.google.Chrome.canary" then
+		if apps[bundleID] then
 			for _, key in ipairs(chrome_keys) do
 				key:enable()
 			end
 		end
 	end
 	if event == hs.application.watcher.deactivated then
-		if bundleID == "com.google.Chrome" or bundleID == "com.google.Chrome.canary" then
+		if apps[bundleID] then
 			for _, key in ipairs(chrome_keys) do
 				key:disable()
 			end
 		end
 	end
+end
+
+for _, key in ipairs(global_keys) do
+	key:enable()
 end
 
 return hs.application.watcher.new(handle_app_event)
